f0536568f560c6d5e71edf5105f6b7029dd1ce8a
CordBase <144811064+cordbase@users.noreply.github.com>
2025-09-28 20:52:27 +0530

    Update and rename lineage_PacmanPro.mk to clover_PacmanPro.mk

--- clover_PacmanPro.mk
+++ clover_PacmanPro.mk
@@ -6,14 +6,14 @@
 # Inherit from the custom device configuration.
 $(call inherit-product, device/nothing/Aerodactyl/device-PacmanPro.mk)
 
-# Inherit from the LineageOS configuration.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from the Clover configuration.
+$(call inherit-product, vendor/clover/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Nothing
 PRODUCT_DEVICE := PacmanPro
 PRODUCT_MANUFACTURER := Nothing
 PRODUCT_MODEL := A142P
-PRODUCT_NAME := lineage_PacmanPro
+PRODUCT_NAME := clover_PacmanPro
 
 PRODUCT_GMS_CLIENTID_BASE := android-nothing
 
