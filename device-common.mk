a4aed3f03ce49ed5df4916f69510226c09d55d40
CordBase <144811064+cordbase@users.noreply.github.com>
2025-09-30 01:18:57 +0530

    Update device-common.mk

--- device-common.mk
+++ device-common.mk
@@ -149,9 +149,6 @@ PRODUCT_PACKAGES += \
 
 $(call soong_config_set,lineage_health,charging_control_charging_path,/proc/charger/usb_charger_en)
 
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
 # Init
 PRODUCT_PACKAGES += \
     fstab.enableswap \
