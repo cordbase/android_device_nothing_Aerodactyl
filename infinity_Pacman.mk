c4ffc9474545c18095a0e62a6d35ce862d40b83a
CordBase <144811064+cordbase@users.noreply.github.com>
2025-09-30 01:00:57 +0530

    Update and rename lineage_Pacman.mk to infinity_Pacman.mk

--- infinity_Pacman.mk
+++ infinity_Pacman.mk
@@ -6,14 +6,21 @@
 # Inherit from the custom device configuration.
 $(call inherit-product, device/nothing/Aerodactyl/device-Pacman.mk)
 
-# Inherit from the LineageOS configuration.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from the Infinityx configuration.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+
+INFINITY_BUILD_TYPE := UNOFFICIAL
+INFINITY_MAINTAINER := "Himanshu"
+TARGET_SUPPORTS_BLUR := true
+WITH_GAPPS := true
+TARGET_SHIPS_FULL_GAPPS := true
+TARGET_SHIPS_GOOGLE_DIALER := true
 
 PRODUCT_BRAND := Nothing
 PRODUCT_DEVICE := Pacman
 PRODUCT_MANUFACTURER := Nothing
 PRODUCT_MODEL := A142
-PRODUCT_NAME := lineage_Pacman
+PRODUCT_NAME := infinity_Pacman
 
 PRODUCT_GMS_CLIENTID_BASE := android-nothing
 
