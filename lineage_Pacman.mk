8c94b5316594b340d460a0edb5d863a4c5f72be6
CordBase <144811064+cordbase@users.noreply.github.com>
2025-10-02 20:03:17 +0530

    Update lineage_Pacman.mk

--- lineage_Pacman.mk
+++ lineage_Pacman.mk
@@ -17,6 +17,17 @@ PRODUCT_NAME := lineage_Pacman
 
 PRODUCT_GMS_CLIENTID_BASE := android-nothing
 
+# Lunaris customizations
+TARGET_OPTIMIZED_DEXOPT := true
+TARGET_ENABLE_BLUR := true
+WITH_BCR := true
+WITH_GMS := true
+TARGET_USES_CORE_GAPPS := true
+PERF_ANIM_OVERRIDE := true
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.paranoid.maintainer=Himanshu
+
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="sys_mssi_64_ww_armv82-user 15 AP3A.240905.015.A2 2509041648 release-keys" \
     BuildFingerprint=Nothing/Pacman/Pacman:15/AP3A.240905.015.A2/2509041648:user/release-keys \
