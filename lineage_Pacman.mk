5ebc758061e4f29c1197adc395096b37a5370465
CordBase <144811064+cordbase@users.noreply.github.com>
2025-10-10 13:39:36 +0530

    Update lineage_Pacman.mk

--- lineage_Pacman.mk
+++ lineage_Pacman.mk
@@ -13,7 +13,7 @@ PRODUCT_BRAND := Nothing
 PRODUCT_DEVICE := Pacman
 PRODUCT_MANUFACTURER := Nothing
 PRODUCT_MODEL := A142
-PRODUCT_NAME := lineage_pacman
+PRODUCT_NAME := lineage_Pacman
 
 PRODUCT_GMS_CLIENTID_BASE := android-nothing
 
